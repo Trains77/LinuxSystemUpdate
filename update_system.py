a8922664e9417571050c659491f53b393f12bc98
Trains77 <trains77videos123@gmail.com>
2022-03-21 21:11:49 -0400

    Removed requirement to run with sudo

--- update_system.py
+++ update_system.py
@@ -3,9 +3,8 @@ from pathlib import Path
 # os.getenv("HOME") + "/.librewolf/" + librewolf_instance +  "/favicons.sqlite"
 #isFile = os.path.isfile(librewolf1)
 root_enabled = True
-if not getpass.getuser() == "root":
-    print("This script requires root permissions to run")
-    root_enabled = False
+if getpass.getuser() == "root":
+    print("This script should not be run as root")
 
 # Get file paths to the package managers
 apt_exists = shutil.which("apt")
@@ -15,31 +14,31 @@ flatpak_exists = shutil.which("flatpak")
 dnf_exists = shutil.which("dnf")
 yum_exists = shutil.which("yum")
 crew_exists = shutil.which("crew")
+yay_exists = shutil.which("yay")
 
 # this only exists to provide a gurantee non existant path, which is needed in order to properly detect if the actual package managers exist
 dummy = shutil.which("tgaaoiwaga52agaskgawn/joaguiahdnsllaigwhi")
 
-# print(dummy)
-# print(str(flatpak_exists))
-
 # Run update programs
 if root_enabled == True:
-    if not pacman_exists == dummy: # Pacman
-        os.system(str(pacman_exists) + " -Syu")
+    if not yay_exists == dummy: # Yay
+        os.system(str(yay_exists) + " -Syu")
+    elif not pacman_exists == dummy: # Pacman
+        os.system("sudo " + str(pacman_exists) + " -Syu")
     elif not apt_exists == dummy: # Apt
-        os.system(str(apt_exists) + " update")
-        os.system(str(apt_exists) + " upgrade")
+        os.system("sudo " + str(apt_exists) + " update")
+        os.system("sudo " + str(apt_exists) + " upgrade")
     elif not dnf_exists == dummy: # Dnf
-        os.system(str(dnf_exists) + " clean all")
-        os.system(str(dnf_exists) + " check-update")
-        os.system(str(dnf_exists) + " update")
+        os.system("sudo " + str(dnf_exists) + " clean all")
+        os.system("sudo " + str(dnf_exists) + " check-update")
+        os.system("sudo " + str(dnf_exists) + " update")
     elif not crew_exists == dummy: # Chromebrew
-        os.system(str(crew_exists) + "su chronos -c '" + str(crew_exists) + " update'")
-        os.system(str(crew_exists) + "su chronos -c '" + str(crew_exists) + " upgrade'")
+        os.system(str(crew_exists) + str(crew_exists) + " update'")
+        os.system(str(crew_exists) + str(crew_exists) + " upgrade'")
     elif not yum_exists == dummy: # Yum
-        os.system(str(yum_exists) + " clean all")
-        os.system(str(yum_exists) + " check-update")
-        os.system(str(yum_exists) + " update")
+        os.system("sudo " + str(yum_exists) + " clean all")
+        os.system("sudo " + str(yum_exists) + " check-update")
+        os.system("sudo " + str(yum_exists) + " update")
     #
     if not flatpak_exists == dummy: # Flatpak
         os.system(str(flatpak_exists) + " update")
